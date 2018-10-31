a3256ff6623999951496d275471a35e52b23a62e
Sourabh Balgi <sourabhbalgi@gmail.com>
2018-10-31 19:36:12 +0530

    log files for the run updated

--- part1_logreg_local/logreg_local_sgd.py
+++ part1_logreg_local/logreg_local_sgd.py
@@ -2,15 +2,39 @@ import tensorflow as tf
 import numpy as np
 import time
 import h5py
+from argparse import ArgumentParser
+
+
+parser = ArgumentParser()
+parser.add_argument('--epochs', default=50,
+                    help='number of epochs')
+parser.add_argument('--lr_begin', default=0.02,
+                    help='starting learning rate')
+parser.add_argument('--batch_size', default=4000,
+                    help='batch size')
+parser.add_argument('--decay_type', default='const',
+                    help='Type of lr decay (const , inc , dec )')
+parser.add_argument('--train_label_file', default='train_l.npy',
+                    help='Name of file with train labels')
+parser.add_argument('--test_label_file', default='test_l.npy',
+                    help='Name of file with test labels')
+parser.add_argument('--train_tfidf_file', default='train.h5',
+                    help='Name of file with train data features')
+parser.add_argument('--test_tfidf_file', default='test.h5',
+                    help='Name of file with test data features')
+parser.add_argument('--datadir_path', default='/home/sourabhbalgi/prj/DS222_assignment2/part1_logreg_local',
+                    help='Path to folder where preprocessed data is present')
+parser.add_argument('--out_prefix', default='final_',
+                    help='Prefix to output filename')
+args = parser.parse_args()
 
 print(" Data Loading ... \n")
 
-label_train = np.load("train_l.npy").astype(np.float32)
-# tfidf_documents_test=np.load("test.npy").astype(np.float32)
-label_test = np.load("test_l.npy").astype(np.float32)
-h5f1 = h5py.File('train.h5', 'r')
+label_train = np.load(args.datadir_path + '/' + args.train_label_file).astype(np.float32)
+label_test = np.load(args.datadir_path + '/' + args.test_label_file).astype(np.float32)
+h5f1 = h5py.File(args.datadir_path + '/' + args.train_tfidf_file, 'r')
 tfidf_train = h5f1['d1'][:]
-h5f2 = h5py.File('test.h5', 'r')
+h5f2 = h5py.File(args.datadir_path + '/' + args.test_tfidf_file, 'r')
 tfidf_test = h5f2['d2'][:]
 
 print('train data shape: {}'.format(tfidf_train.shape))
@@ -18,9 +42,10 @@ print('train label data shape: {}'.format(label_train.shape))
 
 print('\n Defining the graph ... \n')
 # Parameters
-learning_rate = 0.02
-n_epochs = 54
-batch_size = 4000
+n_epochs = args.epochs
+batch_size = args.batch_size
+lr_begin = args.lr_begin
+global_step = tf.Variable(0)
 
 X = tf.placeholder(tf.float32, shape=[None, tfidf_train.shape[1]], name='image')
 Y = tf.placeholder(tf.float32, shape=[None, 50], name='label')
@@ -36,8 +61,22 @@ entropy = tf.nn.softmax_cross_entropy_with_logits_v2(labels=Y, logits=Z_out, nam
 L = tf.reduce_mean(entropy)
 L2_regularizer = tf.nn.l2_loss(W)
 loss = tf.reduce_mean(L + 0.01 * L2_regularizer)
+n_batches = int(tfidf_train.shape[0]/batch_size)
+
+if args.decay_type == 'const':
+    learning_rate = tf.train.exponential_decay(lr_begin, global_step,
+                                               n_batches, 1, staircase=True)
+elif args.decay_type == 'dec':
+    learning_rate = tf.train.exponential_decay(lr_begin, global_step,
+                                               n_batches, .95, staircase=True)
+elif args.decay_type == 'inc':
+    learning_rate = tf.train.exponential_decay(lr_begin, global_step,
+                                               n_batches, 1.05, staircase=True)
+else:
+    raise ValueError
+
 opt = tf.train.AdamOptimizer(learning_rate=learning_rate)
-optimizer = opt.minimize(loss)
+optimizer = opt.minimize(loss, global_step=global_step)
 predicted_labels = tf.nn.softmax(Z_out)
 
 
@@ -53,7 +92,7 @@ def accuracy(predicted_labels, labels):
 
 
 init = tf.global_variables_initializer()
-writer = tf.summary.FileWriter('./plots/logreg', tf.get_default_graph())
+writer = tf.summary.FileWriter(args.datadir_path + '/' + 'plots/logreg', tf.get_default_graph())
 
 with tf.Session() as sess:
     start = time.time()
@@ -68,6 +107,7 @@ with tf.Session() as sess:
             _, loss_int = sess.run([optimizer, loss], feed_dict={X: x, Y: y})
             total_loss += loss_int
         print('Average loss epoch {0}: {1}'.format(i, total_loss / n_batches))
+        print(learning_rate.eval())
     print('Total time: {0} seconds'.format(time.time() - start))
 
     # test the model
--- part1_logreg_local/train_n_log_accuracies.sh
+++ part1_logreg_local/train_n_log_accuracies.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
-source activate ds222_as1
-echo -e "\n>>> Activated ds222_as1 conda environment . \n"
+# echo -e "\n>>> Preprocessing data . \n"
+# python data_preprocess.py
+# echo -e "\n>>> Preprocessing data done. \n"
 
-echo -e "\n>>> Preprocessing data . \n"
-python data_preprocess.py
-echo -e "\n>>> Preprocessing data done. \n"
+echo -e "\n>>> Running logreg in local with constant learning rate. \n"
+python logreg_local_sgd.py --decay_type 'const'
+echo -e "\n>>> Running logreg in local with constant learning rate done. \n"
 
-echo -e "\n>>> Running logreg in local . \n"
-python logreg_local_sgd.py
-echo -e "\n>>> Running logreg in local done. \n"
+echo -e "\n>>> Running logreg in local with increasing learning rate. \n"
+python logreg_local_sgd.py --decay_type 'dec'
+echo -e "\n>>> Running logreg in local with increasing learning rate done. \n"
 
-source deactivate ds222_as1
-echo -e "\n>>> Deactivated ds222_as1 conda environment . \n"
+echo -e "\n>>> Running logreg in local with decreasing learning rate. \n"
+python logreg_local_sgd.py --decay_type 'inc'
+echo -e "\n>>> Running logreg in local with decreasing learning rate done. \n"
+
